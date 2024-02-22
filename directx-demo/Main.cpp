43525509da45bbf714d52ec5698c2f212e59cbbe
Neo <neo@mail.com>
2024-02-21 23:26:17 -0500

    Added close button

--- directx-demo/Main.cpp
+++ directx-demo/Main.cpp
@@ -3,6 +3,14 @@
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch (uMsg) {
+	case WM_CLOSE: {
+			if (MessageBox(hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
+			{
+				DestroyWindow(hwnd);
+			}
+			// Else: User canceled. Do nothing.
+			return 0;
+		}			
 		case WM_DESTROY: {
 			PostQuitMessage(0);
 			return 0;
@@ -31,11 +39,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline,
 
 	RegisterClass(&wndClass);
 	
-	HWND hWnd = CreateWindowEx(
+	HWND hWnd = CreateWindowExW(
 		0,
 		className,
 		L"Window Title",
-		NULL,
+		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		1280, 720,
 		NULL,
