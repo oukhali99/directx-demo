1c09f406d2bcb5eb0b6d8e8171239d51726ed5bf
Neo <neo@mail.com>
2024-02-21 23:22:07 -0500

    Basic window

---
+++ directx-demo/Main.cpp
@@ -0,0 +1,61 @@
+#include <Windows.h>
+#include <string>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+		case WM_DESTROY: {
+			PostQuitMessage(0);
+			return 0;
+		}
+		case WM_PAINT: {
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+
+			// All painting occurs here, between BeginPaint and EndPaint.
+
+			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+			EndPaint(hwnd, &ps);
+		}
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int nCmdshow) {
+	wchar_t className[] = L"Some Class Name";
+
+	WNDCLASS wndClass = {};
+	wndClass.lpszClassName = className;
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc = WindowProc;
+
+	RegisterClass(&wndClass);
+	
+	HWND hWnd = CreateWindowEx(
+		0,
+		className,
+		L"Window Title",
+		NULL,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		1280, 720,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (hWnd == NULL)
+	{
+		return 0;
+	}
+
+	ShowWindow(hWnd, nCmdshow);
+
+	MSG message;
+	while (GetMessageA(&message, NULL, 0, 0)) {
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+
+	return 0;
+}
